2e48172269c8b2417939a2ee5d3de7bf9304a37d
franciscoalfar@gmail.com <*Fran2811*>
2023-08-31 20:30:51 -0400

    first commit

---
+++ models/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const http = require('http');
+const socketio = require("socket.io");
+const Sockets = require('./sockets');
+const cors = require('cors')
+
+
+class Server{
+    constructor(){
+        this.app = express()
+        this.port = 3000
+        this.server = http.createServer(this.app);
+        
+        this.io = socketio(this.server)
+
+
+        //inicializar socket
+        this.sockets= new Sockets(this.io)
+
+    }
+    middlewares(){
+        this.app.use(cors())
+
+        this.app.get("/ultimos", (req, res) => {
+
+            res.json({
+                ultimos:this.sockets.ticketList.ultimosTrece
+            });
+
+        });
+
+    }
+
+
+    execute(){
+        this.server.listen(this.port, () => {
+            console.log(`holi Servidor en puerto : ${this.port}`);
+          });
+
+        this.middlewares()
+
+    }
+
+
+}
+module.exports = Server;
---
+++ models/sockets.js
@@ -0,0 +1,45 @@
+
+const TicketList = require ('./ticket-list')
+
+class Sockets {
+
+    constructor(io) {
+        this.io = io;
+
+        this.ticketList = new TicketList()
+
+        this.socketEvents()
+
+    }
+
+    socketEvents() {
+        //conexion 
+        this.io.on('connection', (socket) => {
+
+            console.log('cliente conectado')
+
+            socket.on('solicitar-ticket', (data, callback)=>{
+                const nuevoTicket = this.ticketList.crearTicket()
+                callback(nuevoTicket)
+               
+            })
+
+            socket.on('siguiente-ticket', ({agente,escritorio}, callback)=>{
+                const siguientTicket = this.ticketList.asignarTicket(agente,escritorio)
+                callback(siguientTicket)
+
+                this.io.emit('ticket-asignado', this.ticketList.ultimosTrece)
+            })
+
+
+            socket.on('disconnect', () => {
+                console.log('Usuario desconectado');
+
+            });
+        });
+
+
+    }
+
+}
+module.exports = Sockets;
